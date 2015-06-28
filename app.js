8f7c7e9dff90807d8656ef33621620f7f6f79473
urzolaj <johann.gz@gmail.com>
2015-06-28 17:13:15 -0500

    Test Completed

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 var express = require('express');
+var sassMiddleware = require('node-sass-middleware');
 var logger = require('morgan');
 var path = require('path');
 var api = require('./api');
 var app = express();
 
 app.use(logger('dev'));
+
+app.use(sassMiddleware({
+    src: path.join(__dirname, 'public/styles'),
+    dest: path.join(__dirname, 'public/styles'),
+    debug: true,
+    force: true,
+    prefix: '/styles'
+}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', api);
+
+app.get('/:name', function(req, res) {
+  res.sendfile(path.join(__dirname, 'public', 'new.html'));
+});
+
+app.get('/:name1/:name2', function(req, res) {
+  res.sendfile(path.join(__dirname, 'public', 'new.html'));
+});
 
 module.exports = app;
---
+++ public/scripts/main.js
@@ -0,0 +1,125 @@
+container = document.getElementById('container');
+offcanvas = document.getElementById('offcanvas');
+overlay = document.getElementById('overlay');
+
+var xmlhttp = new XMLHttpRequest();
+
+xmlhttp.onreadystatechange = function() {
+	try {
+	    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+	        var data = JSON.parse(xmlhttp.responseText);
+	        handleData(data);
+	    }
+	} catch(e) {
+	    console.log('Opening new page');
+	}
+}
+xmlhttp.open("GET", '/api/nav.json', true);
+xmlhttp.send();
+
+function handleData(navText) {
+
+	function generateMenu(items) {
+		var ul = document.createElement('ul');
+		items.forEach(function(item) {
+
+			var li = document.createElement('li');
+		    var a = document.createElement('a');
+
+		    a.title = item.label;
+		    a.appendChild(document.createTextNode(item.label));
+
+		    if (item.items && item.items.length > 0){
+		    	a.href = item.url;
+		    	li.className = 'dropdown';
+		    	li.setAttribute('onclick','toggleOpen(this)');
+		    	li.appendChild(a);
+		    	li.appendChild(generateMenu(item.items));
+		    }
+		    else{
+		    	if (item.url.substring(0, 1) == '#') { 
+				  item.url = item.url.substring(1);
+				}
+				a.href = item.url;
+		    	a.setAttribute('onclick','openNew(this)');
+		    	li.appendChild(a);
+		    }
+
+		    ul.appendChild(li);
+		});
+		return ul;
+	}
+	var scroller = document.createElement('div');
+	scroller.className = "scroller";
+	menunav = generateMenu(navText.items);
+	menunav.id = 'menunav';
+	document.getElementById('menu').appendChild(menunav);
+	scroller.appendChild(generateMenu(navText.items));
+	var copyText = document.createElement('span');
+	copyText.className = "copy";
+	copyText.innerHTML = '&copy; 2014 Huge. All Rights Reserved.';
+	scroller.appendChild(copyText);
+	offcanvas.appendChild(scroller);
+}
+
+function closeDropdowns(){
+	var dropdowns = document.getElementsByClassName('dropdown');
+	for (var i=0; i < dropdowns.length; i++){
+		if (dropdowns[i].classList.contains("open")) {
+			dropdowns[i].classList.remove("open");
+			break;
+		}
+	}
+}
+
+function toggleMenu() {
+	if (container.className == 'nav') {
+		overlay.className = '';
+		container.className = '';
+		setTimeout(function () {
+			offcanvas.className = '';
+		}, 300);
+	}
+	else {
+		closeDropdowns();
+		overlay.className = 'visible';
+		offcanvas.className = 'visible';
+		container.className = 'nav';
+	}	
+}
+
+function toggleOpen(item) {
+	var className = ' ' + item.className + ' ';
+	fromHeader = false;
+	if (item.parentNode.id == 'menunav') {
+		var fromHeader = true;
+	}
+    if ( ~className.indexOf(' open ') ) {
+        item.className = className.replace(' open ', ' ');
+        if (fromHeader) {
+			overlay.className = '';
+		}
+    } else {
+        if (fromHeader) {
+        	closeDropdowns();
+			overlay.className = 'visible';
+		}
+        item.className += ' open';
+    }
+}
+
+function maskClick(){
+	if (offcanvas.className == 'visible') {
+		toggleMenu();
+	}
+	else{
+		closeDropdowns();
+	}
+	overlay.className = '';
+}
+
+function openNew(item) {
+	console.log('Clicking on '+item.href+' page link');
+	xmlhttp.open("GET",item.href, true);
+	xmlhttp.send();
+}
